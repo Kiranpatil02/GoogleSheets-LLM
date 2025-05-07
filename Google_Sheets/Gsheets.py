6b5cd874ad58a550eb0a91eeddf0f8fc50df441e
Kiran <Kiranpatil73827@gmail.com>
2025-05-07 23:27:05 +0530

    Basic methods testing

--- Google_Sheets/Gsheets.py
+++ Google_Sheets/Gsheets.py
@@ -1,7 +1,46 @@
 import gspread
 gc=gspread.service_account('C:/Users/Kiran Patil/Desktop/For fun/Google_sheets+LLM/Google_Sheets/credintials.json')
 
-sh=gc.open("sheets1")
+sh=gc.open_by_key("1GOqloR-cN1Jq6NFhEDh84BP8OqAMcNv7LJB8jG0SXtg")
 print(sh.sheet1.get("A1"))
 
-sh.update_title("Check-OUT!!")
+gsheet=sh.sheet1
+
+def getvalues(cellnumber):
+    val=sh.sheet1.acell(cellnumber).value
+    print(f"The value of cell ${cellnumber} is :",val)
+
+def updateTitle(titlename):
+    gsheet.update_title("QuizMarks1")
+    print(f"Title of sheet to updated to :",titlename)
+    
+# getvalues("C5")
+def getcolumn_value(row,col):
+    val=gsheet.cell(row,col).value
+    print("The values are:",val)
+
+# getcolumn_value(1,2)
+
+def get_entirecol(col):
+    values_list=gsheet.col_values(2)
+    print("Entire column values are:",values_list)
+    
+
+# get_entirecol(2)
+
+def get_alldetails():
+    lists=gsheet.get_all_values()
+    print(lists)
+
+# get_alldetails()
+
+def get_alldetails_dict():
+    records=gsheet.get_all_records()
+    print(records)
+
+# get_alldetails_dict()
+
+def formatting():
+    gsheet.format('A1',{'textFormat': {'bold': True}})
+
+formatting()
